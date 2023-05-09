821828d3358397f74b75d1cb706c6514495adff7
Ivan Korablin <ikorablin@tehzor.ru>
2023-05-09 03:34:34 +0300

    asdasd

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Icon from './3WsJpu48nLY.jpg';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+				{/* / <img className='img' src={Icon} /> */}
+				<div className='img'></div>
+				<h2 style={{fontSize: '96px'}}>АНТОХА КРУТОЙ</h2>
+				<h3 style={{color: '#2BCC1E'}} >УСЛУГИ:</h3>
+				<br/>
+				<h3 style={{color: '#2BCC1E'}} >ВЫЕХАТЬ ЗА ПИВОМ</h3>
+				<h3 style={{color: '#1ECC9F'}} >свозить В МАК</h3>
+				<h3 style={{color: '#C121D4'}} >уСТРОЙИТЬ	нагоняй ПАТЛАТОМУ</h3>
+				<h3 style={{color: '#C121D4'}} >ВАША УСЛУГА - <a  target="_blank" href='https://web.telegram.org/a/#383988124'>ТЕЛЕГРАМ @toxakrut</a></h3>
+				+7 981 442 1175
+				<br/>
       </header>
     </div>
   );
